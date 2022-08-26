24d25211d9ea02507ee73f62e08af035e4da0240
rafi612 <rafi612lol@gmail.com>
2022-08-26 22:28:35 +0200

    feat: added updateIDInList function

--- src/main/java/com/serverd/plugin/listener/UpdateIDListener.java
+++ src/main/java/com/serverd/plugin/listener/UpdateIDListener.java
@@ -1,5 +1,7 @@
 package com.serverd.plugin.listener;
 
+import java.util.List;
+
 import com.serverd.plugin.Plugin;
 
 /**
@@ -14,4 +16,22 @@ public interface UpdateIDListener
 	 * @param newid New client id
 	 */
 	public void updateID(Plugin plugin,int oldid,int newid);
+	
+	/**
+	 * Updates specific list with ID of clients
+	 * @param list List object to update
+	 * @param oldid Old id of client
+	 * @param newid New id of client
+	 */
+	default void updateIDInList(List<Integer> list,int oldid,int newid)
+	{
+		if (oldid == newid) return;
+		
+        if (list.size() > 0)
+        {
+            int index = list.lastIndexOf(oldid);
+            if (index != -1)
+                list.set(index, newid);
+        }
+	}
 }
