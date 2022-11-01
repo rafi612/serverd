6b6676bd56443674dd1ed9cbfa12646e79e24de4
rafi612 <rafi612lol@gmail.com>
2022-11-01 22:35:45 +0100

    test: updated

--- src/test/java/com/serverd/plugin/DebugTest.java
+++ src/test/java/com/serverd/plugin/DebugTest.java
@@ -1,24 +1,30 @@
 package com.serverd.plugin;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import com.serverd.main.Main;
 import com.serverd.plugin.Plugin.Info;
 
 class DebugTest 
 {	
+	@TempDir
+	File testWorkdir;
 	
 	@Test
 	void loadPluginFromClassName_Test()
 	{
-		assertDoesNotThrow(() -> Debug.loadPluginFromClassName(TestPlugin2.class.getName()));
+		System.setProperty("inside.test", "true");
+		assertDoesNotThrow(() -> Debug.loadPluginFromClassName(DebugTestPlugin.class.getName()));
 	}
 	
 	@Test
-	void testPlugin_Test() 
+	void testPlugin_withoutPluginsEnabled_Test() 
 	{
 		assertDoesNotThrow(() -> {
 			String serverdPath = new File(Debug.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
@@ -26,50 +32,61 @@ class DebugTest
 			
 			System.setProperty("java.class.path", System.getProperty("java.class.path") + ":" + serverdPath + ":" + testPath);
 			
-			assertEquals(Debug.testPlugin(TestPlugin1.class.getName(), false, new String[] {}),0);
+			String[] args = {"--working-loc",testWorkdir.getPath()};
+			assertEquals(Debug.testPlugin(DebugTestPlugin.class.getName(), false, args),0);
 		});
 	}
-
+	
+	@Test
+	void testPlugin_withPluginsEnabled_Test() 
+	{
+		assertDoesNotThrow(() -> {
+			String serverdPath = new File(Debug.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+			String testPath = new File(DebugTest.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+			
+			System.setProperty("java.class.path", System.getProperty("java.class.path") + ":" + serverdPath + ":" + testPath);
+			
+			String[] args = {"--working-loc",testWorkdir.getPath()};
+			assertEquals(Debug.testPlugin(DebugTestPlugin.class.getName(), true, args),0);
+		});
+	}
+	
+	@Test
+	void testPlugin_withArgsNull_Test() 
+	{
+		File defaultWorkdir = new File(Main.getWorkDir());
+		assumeTrue(defaultWorkdir.exists() || defaultWorkdir.mkdir());
+		
+		assertDoesNotThrow(() -> {
+			String serverdPath = new File(Debug.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+			String testPath = new File(DebugTest.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+			
+			System.setProperty("java.class.path", System.getProperty("java.class.path") + ":" + serverdPath + ":" + testPath);
+			
+			assertEquals(Debug.testPlugin(DebugTestPlugin.class.getName(), false, null),0);
+		});
+	}
 }
 
-class TestPlugin1 implements ServerdPlugin
+class DebugTestPlugin implements ServerdPlugin
 {
-	public TestPlugin1() {}
+	public DebugTestPlugin() {}
 	@Override
 	public void metadata(Info info) {}
 
 	@Override
 	public String init(Plugin plugin)
 	{
-		return null;
+		return INIT_SUCCESS;
 	}
 
 	@Override
 	public void work(Plugin plugin) 
 	{
-		System.exit(0);
+		if (System.getProperty("inside.test") == null)
+			System.exit(0);
 	}
 
 	@Override
-	public void stop(Plugin plugin) {}
-	
-}
-
-class TestPlugin2 implements ServerdPlugin
-{
-	public TestPlugin2() {}
-	@Override
-	public void metadata(Info info) {}
-
-	@Override
-	public String init(Plugin plugin)
-	{
-		return null;
-	}
-
-	@Override
-	public void work(Plugin plugin) {}
-
-	@Override
-	public void stop(Plugin plugin) {}
+	public void stop(Plugin plugin) {}	
 }
--- src/test/java/com/serverd/plugin/PluginManagerTest.java
+++ src/test/java/com/serverd/plugin/PluginManagerTest.java
@@ -3,43 +3,151 @@ package com.serverd.plugin;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import com.serverd.main.Main;
+import com.serverd.plugin.Plugin.Info;
 
-class PluginManagerTest {
-
-	@TempDir
-	File tempDir;
-	
+class PluginManagerTest
+{
 	Plugin plugin;
 	int pluginid;
+	
+	@Nested
+	class LoadPluginsFromFile
+	{
+		@TempDir
+		File testPluginDir;
+		
+		File jarFile;
+		
+		@BeforeEach
+		void setUp()
+		{
+			PluginManager.pluginDir = testPluginDir.getPath();
+			jarFile = new File(testPluginDir,"TestPlugin.jar");
+		}
+		
+		@Test
+		void loadPlugins_Test() 
+		{
+			assertDoesNotThrow(PluginManager::loadPlugins);
+		}
+		
+		@Test
+		void loadPlugins_withPluginFile_Test() throws FileNotFoundException, IOException 
+		{
+			createPluginFile(jarFile, true, PluginManagerTestPlugin.class.getName(), true, true);
+			
+			assertDoesNotThrow(PluginManager::loadPlugins);
+		}
+		
+		@Test
+		void loadPlugins_withPluginFilecauseError_Test() throws FileNotFoundException, IOException 
+		{
+			createPluginFile(jarFile, false, "test.class", true, true);
+			
+			assertDoesNotThrow(PluginManager::loadPlugins);
+		}
+		
+		
+		@Test
+		void load_Test() throws FileNotFoundException, IOException 
+		{
+			
+			createPluginFile(jarFile, true, PluginManagerTestPlugin.class.getName(), true, true);
+			
+			assertDoesNotThrow(() -> PluginManager.load(jarFile, true));
+		}
+		
+		@Test
+		void load_notEnabled_Test() throws FileNotFoundException, IOException 
+		{			
+			createPluginFile(jarFile, true, PluginManagerTestPlugin.class.getName(), true, true);
+			
+			assertDoesNotThrow(() -> PluginManager.load(jarFile, false));
+		}
+		
+		@Test
+		void load_pluginMainClassNotExists_Test() throws FileNotFoundException, IOException 
+		{			
+			createPluginFile(jarFile, false, "test.class", true, true);
+			
+			var exception = assertThrows(PluginLoadException.class,() -> PluginManager.load(jarFile, true));
+			assertTrue(exception.getCause() instanceof ClassNotFoundException);
+		}
+		
+		@Test
+		void load_noManifestExists_Test() throws FileNotFoundException, IOException 
+		{			
+			createPluginFile(jarFile, true, PluginManagerTestPlugin.class.getName(), false, false);
+			
+			var exception = assertThrows(PluginLoadException.class,() -> PluginManager.load(jarFile, true));
+			assertTrue(exception.getCause() instanceof Exception);
+		}
+		
+		@Test
+		void load_noManifestPluginMainClassEntry_Test() throws FileNotFoundException, IOException 
+		{			
+			createPluginFile(jarFile, true, PluginManagerTestPlugin.class.getName(), true, false);
+			
+			assertThrows(PluginLoadException.class,() -> PluginManager.load(jarFile, false));
+		}
+		
+		void createPluginFile(File file,boolean mainClass,String mainClassName,boolean hasManifest,boolean mainClassManifest) throws FileNotFoundException, IOException
+		{
+			try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(file)))
+			{
+				if (mainClass)
+				{
+					//plugin class
+					String pluginClassFile = mainClassName.replace(".", "/") + ".class";
+					
+					ZipEntry classZipEntry = new ZipEntry(pluginClassFile);
+					zout.putNextEntry(classZipEntry);
+					
+					byte[] classByte = PluginManagerTest.class.getResourceAsStream("/" + pluginClassFile).readAllBytes();
+					zout.write(classByte, 0, classByte.length);
+					
+					zout.closeEntry();
+				}
+				
+				if (hasManifest) 
+				{
+					//manifest
+					String manifest = "Manifest-Version: 1.0\n" 
+							+ (mainClassManifest ? ("Plugin-Main-Class: " + mainClassName + "\n") : "");
+					ZipEntry manifestZipEntry = new ZipEntry(JarFile.MANIFEST_NAME);
+					zout.putNextEntry(manifestZipEntry);
+					zout.write(manifest.getBytes());
+					zout.closeEntry();
+				}
+			}
+		}
+	}
 	
 	@BeforeEach
 	void setUp() throws Exception
 	{
 		PluginManager.init();
 		
-		pluginid = Debug.loadPluginFromClassName(TestPlugin2.class.getName());
+		pluginid = Debug.loadPluginFromClassName(PluginManagerTestPlugin.class.getName());
 		plugin = PluginManager.getPluginByID(pluginid);
 	}
 	
 	@AfterEach
 	void tearDown() throws Exception
 	{
-		PluginManager.unloadPlugin(plugin);
-	}
-	
-	@Test
-	void loadPlugins_Test() 
-	{
-		Main.workingdir = tempDir.getAbsolutePath();
-		assertDoesNotThrow(PluginManager::loadPlugins);
-		
 		PluginManager.unloadAllPlugins();
 	}
 	
@@ -86,12 +194,27 @@ class PluginManagerTest {
 	
 	@Test
 	void EnablePlugin_Test()
-	{
-		PluginManager.disablePlugin(plugin);
-		assertFalse(plugin.isRunned());
-		
+	{		
 		PluginManager.enablePlugin(plugin);
 		assertTrue(plugin.isRunned());
 	}
 
+}
+
+class PluginManagerTestPlugin implements ServerdPlugin
+{
+	@Override
+	public void metadata(Info info) {}
+
+	@Override
+	public String init(Plugin plugin)
+	{
+		return INIT_SUCCESS;
+	}
+
+	@Override
+	public void work(Plugin plugin) {}
+
+	@Override
+	public void stop(Plugin plugin) {}	
 }
