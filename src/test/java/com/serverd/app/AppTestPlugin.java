a54605d76461fb38bd752a7d9845f15ea2e2c00b
rafi612 <rafi612p@outlook.com>
2023-09-27 19:43:19 +0200

    test: fix

--- src/test/java/com/serverd/app/AppTestPlugin.java
+++ src/test/java/com/serverd/app/AppTestPlugin.java
@@ -13,16 +13,15 @@ public class AppTestPlugin implements ServerdPlugin {
 
 	@Override
 	public String init(Plugin plugin) {
-		return INIT_SUCCESS;
-	}
-
-	@Override
-	public void work(Plugin plugin) {
 		if (System.getProperty("running.app") != null) {
 			plugin.trace("Plugin runned as app");
 			System.setProperty("after.run", "true");	
 		}
+		return INIT_SUCCESS;
 	}
+
+	@Override
+	public void work(Plugin plugin) {}
 
 	@Override
 	public void stop(Plugin plugin) {}	
